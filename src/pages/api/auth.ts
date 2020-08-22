592d0df56f4d5a20825abf6e273c6eac44c7d2bc
Justin Bennett <zephraph@gmail.com>
2020-08-22 13:37:47 -0400

    Add request logging to auth

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -1,10 +1,14 @@
 import { NowRequest, NowResponse } from "@now/node";
 import { verifyCookie } from "../../utils/auth";
+import Logger from "pino-http";
+
+const log = Logger();
 
 /**
  * Verifies if user is allowed to access team nav
  */
 export default function auth(req: NowRequest, res: NowResponse) {
+  log(req, res);
   if (verifyCookie(req, res)) {
     res.writeHead(200, "OK");
     res.end();
